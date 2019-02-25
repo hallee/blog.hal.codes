0e62d617309ef2082bb66d2160fc4d1bdc5ec50d
Hal Lee <hal@hal-lee.com>
2019-02-25 06:59:39 -0500

    Progress on styles, text baselines

--- src/components/Post/PostBody.jsx
+++ src/components/Post/PostBody.jsx
@@ -13,6 +13,7 @@ const PostBody = ({ node, titleLink }) => {
         { title }
       </h2>
       <p>{ node.body }</p>
+      <img src="https://static1.squarespace.com/static/50271a61c4aab6c54f9af5ee/t/568e06910e4c1157fa0d1127/1452148376624/?format=2500w" />
     </article>
   );
 };
