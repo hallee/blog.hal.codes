1cd49ac7aac9650c896241420417831e2b0449ae
Hal Lee <hal@hal-lee.com>
2019-03-05 21:30:06 -0500

    Minor fixes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
 import 'normalize.css';
-import './Code.css';
+import './code.css';
 import './Layout.scss';
 import Header from '../Header/Header';
 
