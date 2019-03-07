becdfc1481aea58294e8db90ebb2799ffdf01c1e
Hal Lee <hal@lee.me>
2019-03-06 21:16:42 -0500

    Clean up deps + lintfix

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,8 +32,6 @@ module.exports = {
         },
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     // 'gatsby-plugin-offline',
     'gatsby-plugin-sitemap',
     'gatsby-plugin-sass',
