ffe8b6e35423f25ad260fd099a4ade877bee8c1c
Hal Lee <hal@lee.me>
2019-03-12 08:11:45 -0400

    Removed duplicate query; pass data on page gen

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
 
 import Layout from '../Layout/Layout';
 import SEO from '../SEO/SEO';
 import PostList from '../PostList/PostList';
 import Pagination from '../Pagination/Pagination';
 
-const IndexTemplate = ({ data, pageContext }) => {
+const IndexTemplate = ({ pageContext }) => {
   const {
+    data,
     currentPage,
     prevPagePath,
     nextPagePath,
@@ -34,42 +34,8 @@ const IndexTemplate = ({ data, pageContext }) => {
   );
 };
 
-export const query = graphql`
- query BlogPostQuery($perPage: Int!, $currentPage: Int!) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    blog {
-      blogPosts(per: $perPage, page: $currentPage) {
-        nodes {
-          title
-          kicker
-          body {
-            html
-          }
-          preview {
-            html
-          }
-          slug
-          meta {
-            published
-          }
-        }
-      }
-    }
-  }
-`;
-
-IndexTemplate.defaultProps = {
-  data: null,
-  pageContext: null,
-};
-
 IndexTemplate.propTypes = {
-  data: PropTypes.shape(PropTypes.object),
-  pageContext: PropTypes.shape(PropTypes.object),
+  pageContext: PropTypes.shape(PropTypes.object).isRequired,
 };
 
 export default IndexTemplate;
--- src/generation/create-pages.js
+++ src/generation/create-pages.js
@@ -3,10 +3,12 @@ const path = require('path');
 const createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
+  const perPage = 2;
+
   const pages = await graphql(`
     {
       blog {
-        blogPosts(per: 2) {
+        blogPosts(per: ${perPage}) {
           pageInfo {
             size
             total
@@ -20,10 +22,34 @@ const createPages = async ({ graphql, actions }) => {
   const pageCount = Math.max(total, 1);
 
   for (let i = 1; i <= pageCount; i += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    const pageQuery = await graphql(`
+      {
+        blog {
+          blogPosts(per: ${perPage}, page: ${i}) {
+            nodes {
+              title
+              kicker
+              body {
+                html
+              }
+              preview {
+                html
+              }
+              slug
+              meta {
+                published
+              }
+            }
+          }
+        }
+      }
+    `);
     createPage({
       path: i === 1 ? '/' : `/page/${i}`,
       component: path.resolve('./src/components/Index/Index.jsx'),
       context: {
+        data: pageQuery.data,
         currentPage: i,
         perPage: size,
         prevPagePath: i <= 2 ? '/' : `/page/${i - 1}`,
