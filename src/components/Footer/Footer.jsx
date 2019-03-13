5e269c868ddea9053cf3897ac729bf28b22ef499
Hal Lee <hal@hal-lee.com>
2019-03-13 08:18:53 -0400

    Footer mostly from hal.codes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => (
+  <footer>
+    <ul>
+      <li><span class="emoji">👋</span></li>
+      <li><a href="https://twitter.com/hal_lee">Twitter</a></li>
+      <li><a href="https://github.com/hallee">Github</a></li>
+    </ul>
+  </footer>
+);
+
+export default Footer;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,6 +5,7 @@ import { StaticQuery, graphql } from 'gatsby';
 import 'normalize.css';
 import './Layout.scss';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -21,7 +22,7 @@ const Layout = ({ children }) => (
       <>
         <Header siteTitle={ data.site.siteMetadata.title } />
         <main>{children}</main>
-        <footer />
+        <Footer />
       </>
     ) }
   />
