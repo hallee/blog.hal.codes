fda83d193fba0cdfed2f100d5894d1aca248da16
Hal Lee <hal@hal-lee.com>
2019-03-22 07:30:19 -0400

    404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+
+const NotFound = () => (
+  <Layout>
+    <section>
+      <h1>404</h1>
+    </section>
+  </Layout>
+);
+
+export default NotFound;
