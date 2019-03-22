2480f35792e0d5628b3cee2afb8851ca76f2af12
Hal Lee <hal@lee.me>
2019-03-22 08:04:46 -0400

    Nice dog themed 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Layout from '../components/Layout/Layout';
 
 const NotFound = () => (
   <Layout>
     <section>
-      <h1>404</h1>
+      <article>
+        <h1 style={ { margin: 0, 'text-align': 'center' } }>404</h1>
+        <p>
+          Whatever you’re looking for doesn’t exist. Keep sniffing around! Or&nbsp;
+          <Link to="/">
+            go home →
+          </Link>
+        </p>
+        <p><img src="https://s3.amazonaws.com/f.hal.codes/DSC04791-2.jpg" alt="" /></p>
+      </article>
     </section>
   </Layout>
 );
